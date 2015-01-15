a81f8b6046916c110983ea5fe3b0b0d9a6c21971
Chase Martyn <cmartyn@gmail.com>
2015-01-14 18:22:37 -0800

    Authenticate new users without requiring email confirmation

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -30,20 +30,20 @@ module DeviseTokenAuth
               redirect_url: params[:confirm_success_url]
             })
 
-          else
-            # email auth has been bypassed, authenticate user
-            @client_id = SecureRandom.urlsafe_base64(nil, false)
-            @token     = SecureRandom.urlsafe_base64(nil, false)
-
-            @resource.tokens[@client_id] = {
-              token: BCrypt::Password.create(@token),
-              expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
-            }
-
-            @resource.save!
-
-            update_auth_header
           end
+          
+          # email auth has been bypassed, authenticate user
+          @client_id = SecureRandom.urlsafe_base64(nil, false)
+          @token     = SecureRandom.urlsafe_base64(nil, false)
+
+          @resource.tokens[@client_id] = {
+            token: BCrypt::Password.create(@token),
+            expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
+          }
+
+          @resource.save!
+
+          update_auth_header
 
           render json: {
             status: 'success',
